9ab6f50e663fea08efa7c3ef3f3ec017c89e38c6
Mike Petrault <mike.petrault@univ-lille.fr>
2025-12-15 13:30:31 +0100

    Fill teachings

--- js/data/enseignements.js
+++ js/data/enseignements.js
@@ -1,174 +1,42 @@
 const all_enseignements =  [
     {
-        "title": "Programmation numérique",
-        "year": 2022,
-        "study_year":"IS3",
-        "where":"Polytech Lille",
-        "HTD":46,
-        "heures":{
-            "CM":0,
-            "CTD":8,
-            "TD":0,
-            "TP":36,
-            "Projet":0
-        }
-    },
-    {
-        "title": "Machine Learning",
-        "year": 2022,
-        "study_year":"IS4",
-        "where":"Polytech Lille",
-        "HTD":18,
-        "heures":{
-            "CM":0,
-            "CTD":0,
-            "TD":0,
-            "TP":10,
-            "Projet":10
-        }
-    },
-    {
-        "title": "Programmation numérique",
-        "year": 2023,
-        "study_year":"IS3",
-        "where":"Polytech Lille",
-        "HTD":16,
-        "heures":{
-            "CM":0,
-            "CTD":0,
-            "TD":0,
-            "TP":16,
-            "Projet":0
-        }
-    },
-    {
-        "title": "Algèbre linéaire numérique",
-        "year": 2023,
-        "study_year":"IS3",
-        "where":"Polytech Lille",
-        "HTD":20,
-        "heures":{
-            "CM":0,
-            "CTD":0,
-            "TD":0,
-            "TP":20,
-            "Projet":0
-        }
-    },
-    {
-        "title": "Base de données",
-        "year": 2023,
-        "study_year":"PEIP2",
-        "where":"Polytech Lille",
-        "HTD":21,
-        "heures":{
-            "CM":0,
-            "CTD":0,
-            "TD":9,
-            "TP":12,
-            "Projet":0
-        }
-    },
-    {
-        "title": "Encadrement mémoire de master",
-        "year": 2023,
-        "study_year":"Master Machine Learning",
+        "title": "Algorithmique et Programmation",
+        "year": 2024,
+        "study_year":"PEIP1",
         "where":"Université de Lille",
-        "HTD":2,
+        "HTD":36,
         "heures":{
             "CM":0,
             "CTD":0,
-            "TD":0,
-            "TP":0,
-            "Projet":0
-        }
-    },
-    {
-        "title": "Encadrement projet de master",
-        "year": 2023,
-        "study_year":"Master Machine Learning",
-        "where":"Université de Lille",
-        "HTD":2,
-        "heures":{
-            "CM":0,
-            "CTD":0,
-            "TD":0,
-            "TP":0,
-            "Projet":0
-        }
-    },
-
-
-    {
-        "title": "Bases de données relationnelles",
-        "year": 2024,
-        "study_year":"IS3",
-        "where":"Polytech Lille",
-        "HTD":8,
-        "heures":{
-            "CM":0,
-            "CTD":0,
-            "TD":8,
-            "TP":0,
-            "Projet":0
-        }
-    },
-    
-    {
-        "title": "Systèmes d'information objets",
-        "year": 2024,
-        "study_year":"IS4",
-        "where":"Polytech Lille",
-        "HTD":2,
-        "heures":{
-            "CM":0,
-            "CTD":0,
-            "TD":0,
-            "TP":2,
-            "Projet":0
-        }
-    },
-
-    {
-        "title": "Graphes",
-        "year": 2024,
-        "study_year":"IS3A",
-        "where":"Polytech Lille",
-        "HTD":16,
-        "heures":{
-            "CM":0,
-            "CTD":0,
-            "TD":16,
-            "TP":0,
-            "Projet":0
-        }
-    },
-
-    {
-        "title": "Structures de données et algorithmes",
-        "year": 2024,
-        "study_year":"IS3A",
-        "where":"Polytech Lille",
-        "HTD":18,
-        "heures":{
-            "CM":0,
-            "CTD":0,
-            "TD":0,
+            "TD":18,
             "TP":18,
             "Projet":0
         }
     },
-
     {
-        "title": "Programmation par objets",
-        "year": 2024,
-        "study_year":"IS3",
-        "where":"Polytech Lille",
+        "title": "Algorithme et Démonstration",
+        "year": 2025,
+        "study_year":"Licence 1",
+        "where":"Université de Lille",
         "HTD":18,
         "heures":{
             "CM":0,
             "CTD":0,
-            "TD":0,
+            "TD":18,
+            "TP":0,
+            "Projet":0
+        }
+    },
+    {
+        "title": "Algorithmique et Programmation",
+        "year": 2026,
+        "study_year":"PEIP1",
+        "where":"Université de Lille",
+        "HTD":36,
+        "heures":{
+            "CM":0,
+            "CTD":0,
+            "TD":18,
             "TP":18,
             "Projet":0
         }
